49614905b88cec7cfcb709c7be0b3752bbc489bc
jinu <wlsdn147@naver.com>
2024-06-14 16:52:30 +0900

    Lecture 39 Finished

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldSpringContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldSpringContextLauncherApplication.java
@@ -1,11 +1,15 @@
 package com.in28minutes.learnspringframework.examples.c1;
 
 import java.util.Arrays;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+
+
 
 @Configuration
 @ComponentScan
